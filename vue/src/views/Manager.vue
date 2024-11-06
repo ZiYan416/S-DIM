2b65bfef56887320df5ae1acb0595b5d74c1b252
hlt <2921807740@qq.com>
2024-11-07 00:14:43 +0800

    update .gitignore

--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -4,7 +4,7 @@
     <div class="manager-header">
       <div class="manager-header-left">
         <img style="width: 46px; height: 46px" src="@/assets/imgs/logo.png" />
-        <div class="title"><a style="font-family: 宋体;">奋进</a>后台管理系统</div>
+        <div class="title"><a style="font-family: 宋体;">原神</a>后台管理系统</div>
       </div>
 
       <div class="manager-header-center">
